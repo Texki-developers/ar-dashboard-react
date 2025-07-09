c3bc1822a78e9676dec906495e192b64c270c02e
manshadchangampalli <manshadchangampalli010@gmail.com>
2025-07-10 00:52:21 +0530

    the hover effect added on each row

--- src/builders/table-builder/Table.tsx
+++ src/builders/table-builder/Table.tsx
@@ -3,45 +3,45 @@ import type { ITableColumn } from "../../models/table-column";
 import RenderCell from "./renderCell";
 
 export interface ITableProps {
-  columns: ITableColumn[];
-  data: any[];
+    columns: ITableColumn[];
+    data: any[];
 }
 
 const Table = ({ columns, data }: ITableProps) => {
-  return (
-    <div className="bg-white py-3  rounded-xl">
-      <table className="w-full overflow-auto">
-        <thead className="border-b border-b-borderLightGray ">
-          <tr>
-            {columns.map((column) => (
-              <th
-                key={column.key}
-                className=" py-3 px-6 font-medium whitespace-nowrap text-start">
-                {column.columnName}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data?.map((item: any, i) => {
-            return (
-              <tr className={`${i % 2 === 1 ? "bg-tableBg" : ""} `}>
-                {columns.map((column) => {
-                  return (
-                    <td
-                      key={column.key}
-                      className=" py-3 px-6 font-normal">
-                      {RenderCell({ column, item })}
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+    return (
+        <div className="bg-white py-3  rounded-xl">
+            <table className="w-full overflow-auto">
+                <thead className="border-b border-b-borderLightGray ">
+                    <tr>
+                        {columns.map((column) => (
+                            <th
+                                key={column.key}
+                                className=" py-3 px-6 font-medium whitespace-nowrap text-start">
+                                {column.columnName}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data?.map((item: any, i) => {
+                        return (
+                            <tr className={`${i % 2 === 1 ? "bg-tableBg" : ""}  hover:bg-gray-100`}>
+                                {columns.map((column) => {
+                                    return (
+                                        <td
+                                            key={column.key}
+                                            className=" py-3 px-6 font-normal">
+                                            {RenderCell({ column, item })}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 export default Table;
